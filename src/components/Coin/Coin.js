376dc23ac26bf5c16d666d7d23abd4f5231aa09b
Taha Yıldırım <thyldrm23@gmail.com>
2021-05-12 18:19:42 +0300

    odev yapıldı

--- src/components/Coin/Coin.js
+++ src/components/Coin/Coin.js
@@ -9,10 +9,12 @@ const Coin = (props) => {
       <div className={`Coin ${props.flipping ? "Coin-rotate" : ""}`}>
         <img
           src={CoinTura}
+          alt=""
           className={props.side === "yazi" ? "Coin-back" : "Coin-front"}
         />
         <img
           src={CoinYazi}
+          alt=""
           className={props.side === "yazi" ? "Coin-front" : "Coin-back"}
         />
       </div>
--- src/components/CoinFlipper/CoinFlipper.js
+++ src/components/CoinFlipper/CoinFlipper.js
@@ -10,13 +10,29 @@ class CoinFlipper extends Component {
     this.state = {
       side: "tura",
       flipping: false,
+      toplam:0,
+      yazi:0,
+      tura:0
     };
   }
   handleClick = () => {
     // "At!" butonuna tıkladığımızda paranın dönmesini istiyoruz, bu yüzden "flipping" durumunu "true" yapıyoruz.
     this.setState({ flipping: true });
     // 1 saniye kadar dönmesi yeterli, bu yüzden 1 saniye sonra "flipping" durmunu tekrar "false" yapıyoruz.
+    
     setTimeout(() => this.setState({ flipping: false }), 1000);
+
+    var randomNumber = Math.random() //0 ie 1 arası sayı üretir
+    if(randomNumber>0.5){
+      this.setState({toplam: this.state.toplam + 1});
+      this.setState({tura: this.state.tura + 1});
+      this.setState({side: "tura"});
+    }else {
+      this.setState({toplam: this.state.toplam + 1});
+      this.setState({yazi: this.state.yazi + 1});
+      this.setState({side: "yazi"});
+    }
+
   };
 
   render() {
@@ -27,10 +43,10 @@ class CoinFlipper extends Component {
         <button onClick={this.handleClick}>At!</button>
         <p>
           Toplam
-          <strong> 5 </strong>
+          <strong> {this.state.toplam} </strong>
           atıştan
-          <strong> 3 </strong>ü tura
-          <strong> 2 </strong>
+          <strong> {this.state.tura} </strong>ü tura
+          <strong> {this.state.yazi} </strong>
           si yazı geldi.
         </p>
       </div>
